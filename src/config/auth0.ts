e013e02bafb661adf15267e8688276946b3688ce
Kieve Chua <kievechua@gmail.com>
2017-05-03 23:03:00 +0800

    Initial commit

---
+++ src/config/auth0.ts
@@ -0,0 +1,11 @@
+// https://github.com/auth0/node-auth0
+export default {
+  managementClient: {
+    // token: '{YOUR_API_V2_TOKEN}',
+    // domain: '{YOUR_ACCOUNT}.auth0.com'
+  },
+  authenticationClient: {
+    // domain: '{YOUR_ACCOUNT}.auth0.com',
+    // clientId: '{OPTIONAL_CLIENT_ID}'
+  }
+}
